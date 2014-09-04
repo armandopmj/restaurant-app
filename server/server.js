bdd05c968773fb87a18101df84df4c70184ed9c6
Armando Perez <armandopmj@gmail.com>
2014-09-04 13:06:55 -0700

    Added PubNub to Server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@
 var http = require("http");
 var url = require('url');
 var sendResponse = require('./helpers').sendResponse;
-var getRestaurants = require('./queries').getRestaurants;
+// var getRestaurants = require('./queries').getRestaurants; // D: ERROR WITH CONNECT!!
+var getRestaurants = function(){console.log('hey')}; // D: ERROR WITH CONNECT!!
 
 ///////////////////////////////////////////////////////
 // set up server config
@@ -42,3 +43,43 @@ var server = http.createServer(router);
 server.listen(port, ip);
 
 console.log("Listening");
+
+///////////////////////////////////////////////////////
+// D: PubNub
+var pubnub = require("pubnub").init({
+    publish_key   : "pub-c-2c4e8ddb-7e65-4123-af2d-ef60485170d4",
+    subscribe_key : "sub-c-693a352e-3394-11e4-9846-02ee2ddab7fe"
+});
+
+/* ---------------------------------------------------------------------------
+Publish Messages
+--------------------------------------------------------------------------- */
+var message = { "some" : "data" };
+pubnub.publish({ 
+    channel   : 'my_channel',
+    message   : message,
+    callback  : function(e) { console.log( "SUCCESS!", e ); },
+    error     : function(e) { console.log( "FAILED! RETRY PUBLISH!", e ); }
+});
+
+//  ---------------------------------------------------------------------------
+// Listen for Messages
+// --------------------------------------------------------------------------- 
+pubnub.subscribe({
+    channel  : "my_channel",
+    callback : function(message) {
+        console.log( " > ", message );
+    }
+});
+
+/* ---------------------------------------------------------------------------
+Type Console Message
+--------------------------------------------------------------------------- */
+var stdin = process.openStdin();
+stdin.on( 'data', function(chunk) {
+    pubnub.publish({
+        channel : "my_channel",
+        message : ''+chunk
+    });
+});
+
